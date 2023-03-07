ff83ae625de029ef56749f6bfed5bf013f11062c
faizbyp <faizbyp@gmail.com>
2023-03-07 12:01:58 +0700

    create about page

---
+++ about.php
@@ -0,0 +1,109 @@
+<?php /* Template Name: About */ ?>
+
+<?php get_header();?>
+
+<div class="container-lg">
+  <div class="row py-5">
+    <div class="col-12 col-lg-6">
+      <h1 class="fw-bold">PPKU IPB</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum.<br/><br/>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum</p>
+    </div>
+    <div class="col-12 col-lg-6">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ccr.png" alt="Gedung CCR" class="img-fluid">
+    </div>
+  </div>
+
+  <div class="row-pb-3">
+    <div class="col-12">
+      <nav>
+        <div class="nav nav-tabs nav-justified" id="nav-tab" role="tablist">
+          <button class="nav-link active fw-bold" id="nav-profil-tab" data-bs-toggle="tab" data-bs-target="#nav-profil" type="button" role="tab" aria-controls="nav-profil" aria-selected="true">Profil</button>
+          <button class="nav-link fw-bold" id="nav-tupoksi-tab" data-bs-toggle="tab" data-bs-target="#nav-tupoksi" type="button" role="tab" aria-controls="nav-tupoksi" aria-selected="false">Tupoksi</button>
+          <button class="nav-link fw-bold" id="nav-sejarah-tab" data-bs-toggle="tab" data-bs-target="#nav-sejarah" type="button" role="tab" aria-controls="nav-sejarah" aria-selected="false">Sejarah</button>
+        </div>
+      </nav>
+      <div class="tab-content my-5" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-profil" role="tabpanel" aria-labelledby="nav-profil-tab" tabindex="0">
+          <div class="row text-center">
+            <div class="col-12 col-lg-6 px-4">
+              <p class="fw-bold">Visi</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+            </div>
+            <div class="col-12 col-lg-6 px-4">
+              <p class="fw-bold">Misi</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+            </div>
+          </div>
+          <div class="row pt-4">
+            <div class="col">
+              <p class="fw-bold text-center">Tujuan</p>
+              <ol>
+                <li>Bertaqwa dan beriman kepada Tuhan YME, berwawasan kenegaraan dan kebangsaan.</li>
+                <li>Berfikir logis, sistematis, kuantitatif, dan mampu berkomunikasi ilmiah secara lisan atau tertulis.</li>
+                <li>Kompeten dalam prinsip produksi biomasa dan pengelolaannya untuk keperluan dan kebutuhan manusia.</li>
+                <li>Kompeten dalam prinsip dasar fisika, kimia, dan sosial-ekonomi untuk menguasai aspek pra-produksi, produksi dan pasca produksi biomasa.</li>
+                <li>Memiliki profesionalisme, moral & etika, kepedulian terhadap masyarakat, nilai-nilai dan lingkungan, serta berjiwa wirausaha.</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+        <div class="tab-pane fade" id="nav-tupoksi" role="tabpanel" aria-labelledby="nav-tupoksi-tab" tabindex="0">
+          <div class="row text-center">
+            <div class="col px-4">
+              <p class="fw-bold">Tugas</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+            </div>
+          </div>
+          <div class="row pt-4">
+            <div class="col">
+              <p class="fw-bold text-center">Fungsi</p>
+              <p>Program Pendidikan Kompetensi Umum memiliki fungsi sebagai berikut:</p>
+              <ol>
+                <li>pelaksanaan koordinasi dan pelaksanaan kegiatan pendidikan kompetensi umum bagi mahasiswa;</li>
+                <li>pengendalian mutu pendidikan kompetensi umum sesuai dengan sasaran dan standar mutu yang telah ditetapkan IPB;</li>
+                <li>pembinaan organisasi dan kegiatan kemahasiswaan;</li>
+                <li>pemberian fasilitasi layanan bimbingan konseling bagi mahasiswa;</li>
+                <li>penanganan dan pemeliharaan kebersihanserta ketertiban;</li>
+                <li>pelaksanaan administrasi pendidikan, kepegawaian, keuangan dan aset Program Pendidikan Kompetensi Umum yang terintegrasi dengan administrasi di tingkat IPB.</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+        <div class="tab-pane fade" id="nav-sejarah" role="tabpanel" aria-labelledby="nav-sejarah-tab" tabindex="0">
+          <div class="row">
+            <div class="col-12 col-lg-6 px-4">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ccr2.png" alt="" class="img-fluid">
+            </div>
+            <div class="col-12 col-lg-6 px-4">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum.<br/><br/>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in serunt</p>
+              <button class="btn btn-outline-primary rounded-pill">Selengkapnya <img src="<?php echo get_template_directory_uri(); ?>/assets/icon/arrow-narrow-right.svg" alt=""></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid my-5 py-5" style="background-color: #EDF6FF;">
+  <div class="container-lg">
+    <div class="row">
+      <h2 class="fw-bold">Struktur Organisasi</h2>
+    </div>
+    <div class="row align-items-center py-2">
+      <div class="col-12 col-lg-6">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/pejabat.png" alt="" class="img-fluid">
+      </div>
+      <div class="col-12 col-lg-6">
+        <h5 class="fw-bold">Pejabat PPKU</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h5 class="fw-bold">Dosen Koordinator</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h5 class="fw-bold">Staf Akademik</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer();?>
