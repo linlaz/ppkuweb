2f3914081f0cead1e4f5c974ae8949b13a7df09e
faizbyp <faizbyp@gmail.com>
2023-03-08 22:42:30 +0700

    add sejarah page

---
+++ sejarah.php
@@ -0,0 +1,42 @@
+<?php /* Template Name: Sejarah */ ?>
+
+<?php get_header();?>
+
+<div class="container">
+  <h1 class="fw-bold my-5">Sejarah PPKU IPB</h1>
+  <div class="row">
+    <div class="col">
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ccr.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ccr.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ccr.png" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <div class="row my-5">
+    <div class="col">
+      <p>Di satu sisi, sistem penerimaan yang didasarkan pada penelaahan rapor siswa selama di sekolah menengah merupakan terobosan dalam meratakan kesempatan pendidikan tetapi dari sisi lain menuntut kehati-hatian yang lebih besar karena kesenjangan taraf perkembangan antar-sekolah menengah. Karena itu mahasiswa baru yang diterima oleh IPB, walaupun memiliki kelayakan akademik yang memadai untuk menuntut ilmu di perguruan tinggi, tetapi tidak lepas dari kekurangan latar belakang pengetahuan terutama bagi mereka yang berasal dari sekolah menengah yang agak terbelakang. Akibatnya keragaman latar belakang pengetahuan mereka sangat besar dan IPB memerlukan upaya khusus untuk menangani kelemahan ini.Di samping itu karena IPB merupakan pendidikan tinggi yang basisnya adalah ilmu dasar, maka upaya khusus ini berperan pula untuk memberikan landasan yang relatif sama dan cukup kepada mahasiswa untuk menuntut pendidikan selanjutnya di fakultas masing-masing.Sampai dengan tahun 1993 pemilihan program studi dilakukan setelah mahasiswa lulus TPB sehingga mata kuliah yang diberikan di TPB seragam. Tetapi sejak tahun 1993 pemilihan program studi sudah dapat dilakukan sejak mendaftar ke IPB, dan ini merupakan perubahan mendasar dari sistem pendidikan TPB. Selanjutnya mulai tahun 1995 mahasiswa TPB tidak lagi memperoleh mata kuliah yang seragam, walaupun masih tetap bersifat paket untuk setiap program studi.</p>
+
+      <p>Di satu sisi, sistem penerimaan yang didasarkan pada penelaahan rapor siswa selama di sekolah menengah merupakan terobosan dalam meratakan kesempatan pendidikan tetapi dari sisi lain menuntut kehati-hatian yang lebih besar karena kesenjangan taraf perkembangan antar-sekolah menengah. Karena itu mahasiswa baru yang diterima oleh IPB, walaupun memiliki kelayakan akademik yang memadai untuk menuntut ilmu di perguruan tinggi, tetapi tidak lepas dari kekurangan latar belakang pengetahuan terutama bagi mereka yang berasal dari sekolah menengah yang agak terbelakang. Akibatnya keragaman latar belakang pengetahuan mereka sangat besar dan IPB memerlukan upaya khusus untuk menangani kelemahan ini.Di samping itu karena IPB merupakan pendidikan tinggi yang basisnya adalah ilmu dasar, maka upaya khusus ini berperan pula untuk memberikan landasan yang relatif sama dan cukup kepada mahasiswa untuk menuntut pendidikan selanjutnya di fakultas masing-masing.Sampai dengan tahun 1993 pemilihan program studi dilakukan setelah mahasiswa lulus TPB sehingga mata kuliah yang diberikan di TPB seragam. Tetapi sejak tahun 1993 pemilihan program studi sudah dapat dilakukan sejak mendaftar ke IPB, dan ini merupakan perubahan mendasar dari sistem pendidikan TPB. Selanjutnya mulai tahun 1995 mahasiswa TPB tidak lagi memperoleh mata kuliah yang seragam, walaupun masih tetap bersifat paket untuk setiap program studi.</p>
+    </div>
+  </div>
+</div>
+
+<?php get_footer();?>
