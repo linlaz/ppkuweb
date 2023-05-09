1100f00d200d93ee18c3c7c1d41d59f10e0977af
linlaz <lazuardilintang@apps.ipb.ac.id>
2023-05-09 12:22:55 +0700

    gass

--- about.php
+++ about.php
@@ -2,26 +2,26 @@
 
 <?php get_header();?>
 
-<div class="container-lg">
-  <div class="row py-5">
-    <div class="col-12 col-lg-6">
-      <h1 class="fw-bold">PPKU IPB</h1>
+<div class="container my-5">
+  <div class="row align-items-center py-0 py-md-3 py-lg-5">
+    <div class="col-12 col-lg-6 order-1 order-md-0 my-3">
+      <h1 class="fw-bold text-center text-md-start">PPKU IPB</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum.<br/><br/>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non proident, sunt in serunt mollit anim id est laborum</p>
     </div>
-    <div class="col-12 col-lg-6">
+    <div class="col-12 col-lg-6 order-0 order-md-1 text-center">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ccr.png" alt="Gedung CCR" class="img-fluid">
     </div>
   </div>
 
-  <div class="row-pb-3">
+  <div class="row-py-3">
     <div class="col-12">
-      <nav>
-        <div class="nav nav-tabs nav-justified" id="nav-tab" role="tablist">
-          <button class="nav-link active fw-bold" id="nav-profil-tab" data-bs-toggle="tab" data-bs-target="#nav-profil" type="button" role="tab" aria-controls="nav-profil" aria-selected="true">Profil</button>
-          <button class="nav-link fw-bold" id="nav-tupoksi-tab" data-bs-toggle="tab" data-bs-target="#nav-tupoksi" type="button" role="tab" aria-controls="nav-tupoksi" aria-selected="false">Tupoksi</button>
-          <button class="nav-link fw-bold" id="nav-sejarah-tab" data-bs-toggle="tab" data-bs-target="#nav-sejarah" type="button" role="tab" aria-controls="nav-sejarah" aria-selected="false">Sejarah</button>
-        </div>
-      </nav>
+    <nav>
+          <div class="nav nav-underline nav-justified" id="nav-tab" role="tablist">
+            <button class="nav-link active fw-bold" id="nav-profil-tab" data-bs-toggle="tab" data-bs-target="#nav-profil" type="button" role="tab" aria-controls="nav-profil" aria-selected="true">Profil</button>
+            <button class="nav-link fw-bold" id="nav-tupoksi-tab" data-bs-toggle="tab" data-bs-target="#nav-tupoksi" type="button" role="tab" aria-controls="nav-tupoksi" aria-selected="false">Tupoksi</button>
+            <button class="nav-link fw-bold" id="nav-sejarah-tab" data-bs-toggle="tab" data-bs-target="#nav-sejarah" type="button" role="tab" aria-controls="nav-sejarah" aria-selected="false">Sejarah</button>
+          </div>
+        </nav>
       <div class="tab-content my-5" id="nav-tabContent">
         <div class="tab-pane fade show active" id="nav-profil" role="tabpanel" aria-labelledby="nav-profil-tab" tabindex="0">
           <div class="row text-center">
@@ -88,13 +88,13 @@
 <div class="container-fluid my-5 py-5" style="background-color: #EDF6FF;">
   <div class="container-lg">
     <div class="row">
-      <h2 class="fw-bold">Struktur Organisasi</h2>
+      <h2 class="fw-bold text-center text-md-start">Struktur Organisasi</h2>
     </div>
     <div class="row align-items-center py-2">
       <div class="col-12 col-lg-6">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/pejabat.png" alt="" class="img-fluid">
       </div>
-      <div class="col-12 col-lg-6">
+      <div class="col-12 col-lg-6 my-3 text-center text-md-start">
         <h5 class="fw-bold">Pejabat PPKU</h5>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <h5 class="fw-bold">Dosen Koordinator</h5>
