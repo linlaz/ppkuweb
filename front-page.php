e0f1c651bef66b721db87edd10d062cbb94f2ac2
linlaz <lazuardilintang@apps.ipb.ac.id>
2023-05-09 12:28:57 +0700

    remove d-none

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <div class="banner">
     <div class="banner-content">
         <h1 class="fw-bolder">PPKU IPB</h1>
-        <p class="d-none d-md-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit </p>
+        <p class="d-md-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit </p>
     </div>
 </div>
 
